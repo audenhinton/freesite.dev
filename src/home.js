cf780a5d6492fa8044641698cbc89966311b616a
Auden Hinton <auden@outlook.com>
2021-12-09 08:17:32 -0800

    Updated copy for home.js

--- src/home.js
+++ src/home.js
@@ -10,13 +10,15 @@ const Home = () => {
         <>
         <div className="container-fluid">
             <div className="row">
-                <div className="col-12 p-5">
+                <div className="col-12 p-0 pt-4 p-md-5">
 
                     <div className="container mx-auto">
                         <div className="row">
-                            <div className="col-8 mx-auto">
+                            <div className="col-12 col-md-8 mx-auto">
 
-                                <h1 className="fw-bold">How to setup a free website with SSL through S3 / CloudFront</h1>
+                                <h1 className="mb-4">Hello world!</h1>
+
+                                <a href="https://auden.me/blog/how-to-setup-a-free-website-with-ssl-through-amazon-s3-and-amazon-cloudfront/">Click here to navigate back to the article</a>
 
                             </div>
                         </div>
